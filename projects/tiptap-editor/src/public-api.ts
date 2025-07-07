88376798cb47d670f6c1f9265c50858c813b7e6f
FloGeez <florian.etourneau@gmail.com>
2025-07-07 18:26:51 +0200

    cleaning public api

--- projects/tiptap-editor/src/public-api.ts
+++ projects/tiptap-editor/src/public-api.ts
@@ -2,29 +2,13 @@
  * Public API Surface of tiptap-editor
  */
 
-// Composants principaux
+// Composant principal - seul composant exposé publiquement
 export * from "./lib/tiptap-editor.component";
-export * from "./lib/tiptap-bubble-menu.component";
-export * from "./lib/tiptap-image-bubble-menu.component";
-export { TiptapSlashCommandsComponent } from "./lib/tiptap-slash-commands.component";
-export * from "./lib/toolbar.component";
-export * from "./lib/tiptap-button.component";
-export * from "./lib/tiptap-image-upload.component";
-export * from "./lib/tiptap-separator.component";
 
-// Services
-export * from "./lib/services/image.service";
-export * from "./lib/services/editor-commands.service";
+// Service d'internationalisation
 export * from "./lib/services/i18n.service";
 
-// Extensions
-export * from "./lib/extensions/resizable-image.extension";
-export * from "./lib/extensions/upload-progress.extension";
-
-// Configuration i18n
-export * from "./lib/config/i18n-slash-commands";
-
-// Types et interfaces
+// Types et interfaces pour la configuration
 export type { ToolbarConfig } from "./lib/toolbar.component";
 export type {
   BubbleMenuConfig,
@@ -34,11 +18,6 @@ export type {
   SlashCommandsConfig,
   SlashCommandItem,
 } from "./lib/tiptap-slash-commands.component";
+
+// Configuration par défaut des slash commands
 export { DEFAULT_SLASH_COMMANDS } from "./lib/tiptap-slash-commands.component";
-export type { TiptapButtonConfig } from "./lib/tiptap-button.component";
-export type {
-  ImageUploadResult,
-  ImageData,
-  ResizeOptions,
-} from "./lib/services/image.service";
-export type { ResizableImageOptions } from "./lib/extensions/resizable-image.extension";
