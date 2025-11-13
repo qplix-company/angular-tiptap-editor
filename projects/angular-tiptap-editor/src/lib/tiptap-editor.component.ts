9c04485cf4d78013916c5be8aed3b29c30f2abd0
Alexander <alexander.raudzus@qplix.com>
2025-11-13 18:24:10 +0100

    feat: add readonly class

--- projects/angular-tiptap-editor/src/lib/tiptap-editor.component.ts
+++ projects/angular-tiptap-editor/src/lib/tiptap-editor.component.ts
@@ -127,7 +127,10 @@ export const DEFAULT_TABLE_MENU_CONFIG: TableBubbleMenuConfig = {
   selector: "angular-tiptap-editor",
   exportAs: "angularTiptapEditor",
   standalone: true,
-  host: { "[class.editable]": "this.editable()" },
+  host: { 
+    "[class.editable]": "this.editable()", 
+    "[class.readonly]": "!this.editable()" 
+  },
   hostDirectives: [NoopValueAccessorDirective],
   imports: [
     NgTemplateOutlet,
